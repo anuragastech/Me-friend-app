300b52857257c067bd7c0b8f3ee47a24b292f821
Anurag <anuragraveendren98@gmail.com>
2024-10-21 00:38:56 +0530

    patch added

--- src/app.js
+++ src/app.js
@@ -18,6 +18,43 @@ console.log("data not added");
     }
 })
 
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params.userId;
+    const data = req.body;
+  console.log(userId);
+  
+    try {
+      const ALLOWED_UPDATES = ["firstName", "lastName", "password", "age", "gender", "skill"]; 
+  
+      const isUpdateAllowed = Object.keys(data).every((key) =>
+        ALLOWED_UPDATES.includes(key)
+      );
+  
+      if (!isUpdateAllowed) {
+        return res.status(400).send({ error: "Invalid updates!" });
+      }
+  
+      const user = await User.findByIdAndUpdate(userId, data, {
+        returnDocument:"before",      
+        runValidators: true, 
+
+      });
+  console.log(user);
+  
+      if (!user) {
+        return res.status(404).send({ error: "User not found!" });
+      }
+  
+      res.status(200).send(user); 
+    } catch (error) {
+      res.status(500).send({ error: "Failed to update the user." }); 
+    }
+  });
+  
+
+
+
+
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,52 +1,46 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  
-    firstName: {
+  firstName: {
     type: String,
-    required:true,
+    required: true,
   },
   lastName: {
     type: String,
-    required:true,
-
+    required: true,
   },
   emailId: {
     type: String,
-    required:true,
-    unique:true,
-    lowercase:true,
-    trim:true,
-
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
   },
   password: {
     type: String,
-    required:true,
-
+    required: true,
   },
   age: {
     type: Number,
-    required:true,
-    min:5,
-    max:50,
-
+    required: true,
+    min: 5,
+    max: 50,
   },
   gender: {
     type: String,
-    required:true,
-    validate(value){
-        if(!["male","femail","others"].includes(value)){
-            throw new error("gender data is not valid")
-        }
-    }
-
+    required: true,
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)) {
+        throw new Error("gender data is not valid"); 
+      }
+    },
   },
-  skill:{
-    type:[String],
-  } 
-},{
-    timestamps:true,
-  },);
+  skill: {
+    type: [String],
+  }
+}, {
+  timestamps: true, 
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
