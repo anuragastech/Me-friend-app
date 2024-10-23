35871525f6428d9547fba355f34aeb5a4abb7bc1
Anurag <anuragraveendren98@gmail.com>
2024-10-24 02:21:49 +0530

    user/connection get api created

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -5,20 +5,20 @@ const { userAuth } = require('../middleware/auth');
 const requestReceivedModel=require("../models/connectionRequest");
 const connectionRequestModel = require("../models/connectionRequest");
 
-
+const USER_SAFE_Data=["firstName","lastName","age","photo"]
 
 
 router.get("/user/request/received", userAuth, async (req, res) => {
     try {
       const loggedInUser = req.user;
   
-      const connectionRequests = await connectionRequestModel.findOne({
+      const connectionRequests = await connectionRequestModel.find({
         toUserId: loggedInUser._id,
         status: "interested"
       }).populate("fromUserId", ["firstName", "lastName"]);
   
       if (!connectionRequests) {
-        res.status(400).json({ messege: "Request cannot be found" });
+        res.status(400).json({ message: "Request cannot be found" });
       }
   
       console.log(connectionRequests);
@@ -29,11 +29,39 @@ router.get("/user/request/received", userAuth, async (req, res) => {
       });
   
     } catch (error) {
-      res.status(500).json({ messege: " request cannot find " });
+      res.status(500).json({ message: " request cannot find " });
     }
   });
 
+router.get("/user/connections",userAuth,async(req,res)=>{
+    try{
 
+const loggedInUser=req.user
+const connections=await connectionRequestModel.find({
+ $or:[
+    {fromUserId:loggedInUser,status:"accepted"},
+    {toUserId:loggedInUser,status:"accepted"}
+ ]
+}).populate("fromUSerId",USER_SAFE_Data).populate("toUserId",USER_SAFE_Data)
+
+
+
+if(!connections){
+    res.status(400).json({message:"connections not found "})
+}
+
+const data=connections.map((row)=>{
+    if(row.fromUserId._id.String()===loggedInUser._id.String()){
+        return row.toUserId
+    }
+    row.fromUserId
+})
+
+res.json({messege:"success",data})
+    }catch(error){
+        res.status(500).json({message:"connection not found  error"})
+    }
+})
 
 
 
